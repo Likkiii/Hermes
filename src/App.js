f7153f95f21686276a43068fcd38168528bf10f0
Likhit Ajeesh <likhitajeesh20@gmail.com>
2022-08-25 18:01:53 +0530

    feat: add routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import logo from './logo.svg';
 import Signup from './components/Signup';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 
 const App = () => {
-  return <Signup />;
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<Signup />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
