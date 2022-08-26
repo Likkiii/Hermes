9a7f240eef9cc23f597369abd4b7542a05935acf
Shreyas Bhardwaj <shreyas.bhardwaj@gmail.com>
2022-08-26 22:42:28 +0530

    feat: Login page

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,35 +1,55 @@
 import React from "react";
-import "./Login.css";
+import "../Signup/Signup.css";
+import neon from "../../assets/neon.jpg";
+import {Link} from "react-router-dom";
 
 const Login = () => {
   return (
     <div class="flex items-center justify-center min-h-screen">
-      <div classname="">
-        <div className="header">HERMES</div>
-        <div>
+      <img className="neon relative" src={neon} alt="neon" />
+      <div className="box absolute flex flex-col">
+        <div className="header flex justify-center font-bold text-5xl text-slate-900 rounded-t-lg px-8 py-1">
+          HERMES
+        </div>
+        <div class="flex px-8 py-6 text-left shadow-lg rounded-b-lg bg-slate-900">
           <form action="">
-            <div>
-              <i class="fas fa-user"></i>
-              <label>Username</label>
-              <input
-                type="text"
-                placeholder="Enter your username..."
-                class="border-2 w-full px-4 py-2 focus:outline-none"
-              />
-            </div>
-            <div>
-              <i class="fa-solid fa-key"></i>
-              <label>Password</label>
-              <input
-                type="text"
-                placeholder="Enter your password..."
-                class="border-2 w-full px-5 py-2 focus:outline-none"
-              />
-            </div>
-            <div>
-              <button class="border-2 border-color-black w-full px-6 py-2">
-                Login
-              </button>
+            <div class="mt-4">
+              <div>
+                <i class="text-green-600 fas fa-user"></i>
+                <label
+                  class="block"
+                  className="text-green-500 pl-2"
+                  for="Username"
+                >
+                  Username
+                </label>
+                <input
+                  type="text"
+                  placeholder="Enter your username..."
+                  class="border-b-2 w-full px-4 py-2 text-white mt-2 border-green-600 bg-slate-900 focus:outline-none"
+                />
+              </div>
+              <div class="mt-4">
+                <i class="text-green-600 fa-solid fa-key hover"></i>
+                <label className=" text-green-500 pl-2">Password</label>
+                <input
+                  type="text"
+                  placeholder="Enter your password..."
+                  class="w-full px-4 py-2 mt-2 text-white border-b-2 focus:outline-none border-green-600 bg-slate-900"
+                />
+              </div>
+              <div class="flex">
+                <button class="btn w-full px-6 py-2 mt-4 text-gray-300 font-semibold text-md rounded-lg">
+                  Login
+                </button>
+              </div>
+              <div class="mt-6 text-gray-300">
+                Don't have an account?
+                <Link to="/"
+                className="text-green-500 hover:underline pl-2">
+                  Register
+                </Link>
+              </div>
             </div>
           </form>
         </div>
--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -1,80 +1,84 @@
-import React from 'react';
-import styles from './Signup.css';
-import neon from '../../assets/neon.jpg';
+import React from "react";
+import styles from "./Signup.css";
+import neon from "../../assets/neon.jpg";
+import {Link} from "react-router-dom"
 
 const Signup = () => {
   return (
-    <div class='flex items-center justify-center min-h-screen'>
-      <img className='neon relative' src={neon} alt='neon' />
-      <div className='{styles.box} absolute flex flex-col'>
-        <div className='header flex justify-center font-bold text-5xl text-slate-900 rounded-t-lg px-8 py-1'>
+    <div class="flex items-center justify-center min-h-screen">
+      <img className="neon relative" src={neon} alt="neon" />
+      <div className="box absolute flex flex-col">
+        <div className="header flex justify-center font-bold text-5xl text-slate-900 rounded-t-lg px-8 py-1">
           HERMES
         </div>
-        <div class='flex px-8 py-6 text-left bg-slate-900 shadow-lg rounded-b-lg'>
-          <form action=''>
-            <div class='mt-4'>
+        <div class="flex px-8 py-6 text-left bg-slate-900 shadow-lg rounded-b-lg">
+          <form action="">
+            <div class="mt-4">
               <div>
-                <i class='text-green-600 fas fa-user'></i>
+                <i class="text-green-600 fas fa-user"></i>
                 <label
-                  class='block'
-                  className='text-green-500 pl-2'
-                  for='Username'
+                  class="block"
+                  className="text-green-500 pl-2"
+                  for="Username"
                 >
                   Username
                 </label>
                 <input
-                  type='text'
-                  placeholder='Enter your username...'
-                  class='w-full px-4 py-2 mt-2 text-white border-b-2 focus:outline-none border-green-600 bg-slate-900'
+                  type="text"
+                  placeholder="Enter your username..."
+                  class="w-full px-4 py-2 mt-2 text-white border-b-2 focus:outline-none border-green-600 bg-slate-900"
                 />
               </div>
-              <div class='mt-4'>
-                <i class='text-green-600 fas fa-envelope'></i>
+              <div class="mt-4">
+                <i class="text-green-600 fas fa-envelope"></i>
                 <label
-                  class='block'
-                  className='text-green-500 pl-2'
-                  for='email'
+                  class="block"
+                  className="text-green-500 pl-2"
+                  for="email"
                 >
                   Email
                 </label>
                 <input
-                  type='text'
-                  placeholder='Enter your email...'
-                  class='w-full px-4 py-2 mt-2 text-white border-b-2 focus:outline-none border-green-600 bg-slate-900'
+                  type="text"
+                  placeholder="Enter your email..."
+                  class="w-full px-4 py-2 mt-2 text-white border-b-2 focus:outline-none border-green-600 bg-slate-900"
                 />
               </div>
-              <div class='mt-4'>
-                <i class='text-green-600 fa-solid fa-key hover'></i>
-                <label class='block' className='text-green-500 pl-2'>
+              <div class="mt-4">
+                <i class="text-green-600 fa-solid fa-key hover"></i>
+                <label class="block" className="text-green-500 pl-2">
                   Password
                 </label>
                 <input
-                  type='password'
-                  placeholder='Enter your password...'
-                  class='w-full px-4 py-2 mt-2 text-white border-b-2 focus:outline-none border-green-600 bg-slate-900'
+                  type="password"
+                  placeholder="Enter your password..."
+                  class="w-full px-4 py-2 mt-2 text-white border-b-2 focus:outline-none border-green-600 bg-slate-900"
                 />
               </div>
-              <div class='mt-4'>
-                <label class='block' className='text-green-500'>
+              <div class="mt-4">
+                <label class="block" className="text-green-500">
                   Confirm Password
                 </label>
                 <input
-                  type='password'
-                  placeholder='Enter your password again...'
-                  class='w-full px-4 py-2 mt-2 text-white border-b-2 focus:outline-none border-green-600 bg-slate-900'
+                  type="password"
+                  placeholder="Enter your password again..."
+                  class="w-full px-4 py-2 mt-2 text-white border-b-2 focus:outline-none border-green-600 bg-slate-900"
                 />
               </div>
-              <span class='text-xs text-red-400'>Password must be same!</span>
-              <div class='flex'>
-                <button class='btn w-full px-6 py-2 mt-4 text-gray-300 font-semibold text-md rounded-lg'>
+              <span class="text-xs text-red-400">Password must be same!</span>
+              <div class="flex">
+                <button class="btn w-full px-6 py-2 mt-4 text-gray-300 font-semibold text-md rounded-lg">
                   Create Account
                 </button>
               </div>
-              <div class='mt-6 text-gray-300'>
+              <div class="mt-6 text-gray-300">
                 Already have an account?
-                <a class='text-green-500 hover:underline pl-2' href=''>
+                <Link to="/login"
+                  className="text-green-500 hover:underline pl-2"
+                >
                   Log in
-                </a>
+                </Link>
+                
               </div>
             </div>
           </form>
